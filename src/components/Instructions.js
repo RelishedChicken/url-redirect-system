efdd9d406fec4393731e1a718996de49c68228b2
Veeraj <veerajvadgama@gmail.com>
2020-11-08 15:34:52 +0000

    Updated Content

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -8,9 +8,9 @@ class Instructions extends React.Component{
             <div className="content">
                 <h2 className="pageTitle">Usage</h2>
                 <h3 className="subTitle">Editing a Link</h3>
-                <p>To edit a link, go <b><Link to="/edit/">here</Link></b>.</p>
+                <p>To create a new link, go <b><Link to="/edit/">here</Link></b>.</p>
                 <h3 className="subTitle">Update a Link</h3>
-                <p>To update a link, go <b><Link to="/update/">here</Link></b>.</p>
+                <p>To update an existing link, go <b><Link to="/update/">here</Link></b>.</p>
                 <h3 className="subTitle">To Redirect to a Link</h3>
                 <p>Visit this domain with <b>'/redirect/[uuid]'</b> in the URL.</p>
             </div>
